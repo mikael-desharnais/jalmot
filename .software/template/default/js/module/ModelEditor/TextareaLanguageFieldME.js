0f12ddccf410cfed580c1502af6d00a9ecc1f3e8
Mikael Desharnais <mikael.desharnais@gmail.com>
2013-12-06 16:29:02 +0100

    Modified Sceditor to make it work perfectly

--- .software/template/default/js/module/ModelEditor/TextareaLanguageFieldME.js
+++ .software/template/default/js/module/ModelEditor/TextareaLanguageFieldME.js
@@ -2,14 +2,14 @@ jQuery(document).ready(function(){
 	jQuery('body').live('htmlAppending',function(event,htmlContent){
 		jQuery(htmlContent).find('.HTMLLanguageFieldMEToWYSIWYG').each(function(){
 			var parent=this;
-			var element = jQuery(this).sceditor({
+			jQuery(this).sceditor({
 				plugins: "xhtml",
+				locale : 'fr',
 				emoticonsEnabled : false
 				});
-			var redactor=element.data('redactor');
 		});
 	});
 	jQuery('body').live('closeWindow',function(event,htmlContent){
-		jQuery(htmlContent).find('textarea.HTMLLanguageFieldMEToWYSIWYG').each(function(){ jQuery(this).data('redactor').destroy(); });
+		jQuery(htmlContent).find('textarea.HTMLLanguageFieldMEToWYSIWYG').each(function(){ jQuery(this).data('sceditor').destroy(); });
 	});
 });
